db45f57c14dd274b054498817aa62ca1acba7bee
edir72113-crypto <edir72113@gmail.com>
2026-04-25 10:08:22 -0300

    Update MainTVActivity.kt

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/tv/main/MainTVActivity.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/tv/main/MainTVActivity.kt
@@ -1,9 +1,12 @@
 package com.swordfish.lemuroid.app.tv.main
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import com.swordfish.lemuroid.app.R
 
@@ -12,21 +15,29 @@ class MainTVActivity : FragmentActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        // 1. Carrega o ecrã (ficheiro XML) que esvaziou no passo anterior
         setContentView(R.layout.activity_tv_main)
 
-        // 2. Encontra a sua WebView pelo ID que demos lá no XML
         val myWebView: WebView = findViewById(R.id.arena_retro_webview)
-
-        // 3. Hack essencial: Ativa o Javascript e o LocalStorage para o seu React rodar perfeitamente
         myWebView.settings.javaScriptEnabled = true
         myWebView.settings.domStorageEnabled = true
-
-        // 4. Trava a navegação dentro da aplicação (impede que a TV abra o navegador externo)
         myWebView.webViewClient = WebViewClient()
 
-        // 5. O Grande Momento: Carrega o seu sistema de Fliperama Digital!
+        // 1. A PONTE MÁGICA: Conecta o seu site React ao hardware nativo da TV
+        myWebView.addJavascriptInterface(ArenaRetroNative(this), "ArenaRetroNative")
+
         myWebView.loadUrl("https://gorjetaplus.online/")
+    }
+
+    // 2. Esta classe fica "escutando" os comandos que vêm do seu React
+    inner class ArenaRetroNative(private val context: Context) {
+        
+        @JavascriptInterface
+        fun iniciarJogo(romUrl: String, console: String) {
+            // Quando o React chamar essa função, o Kotlin fará o download da ROM
+            // e vai disparar o motor C++ do Lemuroid. (Faremos essa lógica no próximo passo)
+            
+            // Um aviso só para termos certeza que a ponte funcionou quando testarmos:
+            Toast.makeText(context, "Kotlin recebeu: \$console", Toast.LENGTH_SHORT).show()
+        }
     }
 }
